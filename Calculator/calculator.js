42af403593d28e9eac0b012317dea54b6b7ae0bd
Mohammad Esa <mohammad_esa@Mohammads-MacBook-Pro.local>
2023-07-07 11:54:17 +0530

    balle balle

---
+++ Calculator/calculator.js
@@ -0,0 +1,8 @@
+/*let spinnerWrapper = document.querySelector('.spinner-wrapper');
+
+window.addEventListener('load', function(){
+    spinnerWrapper.style.display= 'none';
+    });*/
+$(window).on("load",function(){
+    $(".progress-wrapper").fadeOut("slow");
+});
---
+++ JS/script.js
@@ -0,0 +1,7 @@
+const numberoftimes = 30
+const nice = "hahaok"
+let word=""
+for(let i = 0; i < numberoftimes; i++ ){
+    word=word+nice
+}
+console.log(word)
